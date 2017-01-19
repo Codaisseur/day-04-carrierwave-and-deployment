a463d9b238260b0e5b86af58c1d3175b385c380b
Miriam Tocino <miriam.tocino@gmail.com>
2017-01-19 08:06:57 +0100

    Add changes in schema after migrating

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20160929144219) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_profiles_on_user_id"
+    t.index ["user_id"], name: "index_profiles_on_user_id", using: :btree
   end
 
   create_table "rooms", force: :cascade do |t|
@@ -73,8 +73,10 @@ ActiveRecord::Schema.define(version: 20160929144219) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "profiles", "users"
+  add_foreign_key "rooms", "users"
 end
